b349977355b6614883e915d61a7c39ee8434883c
Unknown <604karev@gmail.com>
2017-12-06 11:33:22 +0300

    old syntax React list elements

---
+++ iShop/CreatorTable.js
@@ -0,0 +1,27 @@
+var CreatorTable = React.createClass({
+
+    render: function () {
+
+        var elements = [];
+
+        this.props.items.forEach(
+            function (el) {
+                return (
+                    elements.push(
+                        React.DOM.div({key: el.id, className: 'element'},
+                            React.DOM.img({src: el.img, alt: el.name}),
+                            React.DOM.div({className: 'elementName'},
+                                React.DOM.b({className: 'bold'}, el.name)),
+                            React.DOM.div({className: 'quantity'}, 'Колличество: ' + el.quantity),
+                            React.DOM.div({className: 'price'}, 'Цена: ' + el.price)
+                        )
+                    )
+                )
+            }
+        );
+
+        return React.DOM.div({className: 'table'},
+            React.DOM.div({className: 'elements'}, elements));
+    },
+
+});
